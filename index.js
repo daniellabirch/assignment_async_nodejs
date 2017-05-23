4fa7823f3c36b1ce1f6b606365b1dbb9622de201
daniella birch <daniellabirch@gmail.com>
2017-05-23 11:28:18 -0600

    almost done!
    almost done with the NodeJS exercises!

---
+++ index.js
@@ -0,0 +1,70 @@
+//1
+var p = new Promise(function(resolve) {
+ setTimeout(function(){
+  resolve("Hello Promise!");
+ },1000);
+});
+
+p.then(function(message) {
+  // 1 second delay
+  console.log(message); //=> Hello Promise!
+});
+
+//2
+function delay(milliseconds){
+	return new Promise(function(resolve){
+		setTimeout(function(){
+			if(milliseconds>0){
+				resolve(milliseconds);
+			}
+		},milliseconds);
+	})
+}
+
+function countDown(number){
+	delay(number)
+		.then(function(i){
+			for(i=number;i>-1;i=i-100){
+				if(i==0){
+					console.log("Done!");
+				}
+				else{
+					console.log(i)
+				};
+			};
+		});
+};
+
+countDown(1000);
+
+//3
+var integers=[1,2,3,4,5,6,7,8,9]
+
+function square(number){
+	return new Promise(function(resolve,reject){
+		if(isNaN(number)==true){
+			reject(number+"is not a number");
+		}
+		else{
+			resolve(number*number);
+		}
+	})
+}
+
+var promisesArray=integers.map(square);
+
+Promise.all(promisesArray)
+  .then(function(results) {
+    console.log(results);
+  });
+
+function doBadThing(forRealz){
+	return new Promise(function(resolve,reject){
+		if(forRealz==false){
+			resolve("Yay");
+		}
+		else if(forRealz==true){
+			reject("forRealz has been rejected");
+		};
+	});
+};
